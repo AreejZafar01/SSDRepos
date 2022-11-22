646b6fa850aac1d3bb1c2b5f357783a89ca1e81a
AreejZafar01 <114142759+AreejZafar01@users.noreply.github.com>
2022-11-22 15:50:19 +0500

    Update i191788_lab11SSD.cpp

--- i191788_lab11SSD.cpp
+++ i191788_lab11SSD.cpp
@@ -1,97 +1,19 @@
-#include<iostream>
+// C++ program to Demonstrate do-while loop
+#include <iostream>
 using namespace std;
-
-class billAmountTracker {
-private:
-	int BillAmount;
-public:
-	billAmountTracker(int BA)
-	{
-		this->BillAmount = BA;
-	}
-	int get_billAmountTracker()
-	{
-		return BillAmount;
-	}
-};
-	class billApprovalchain {
-	public:
-		virtual void setNext(billApprovalchain* NextObj) = 0;
-		virtual void billProcessor(billAmountTracker* Requestedamount) = 0;
-	};
-
-class Normal_CSR: public billApprovalchain {
-private:
-	billApprovalchain* NextObj;
-public:
-	void setNext(billApprovalchain* obj)
-	{
-		NextObj = obj;
-	}
-	void billProcessor(billAmountTracker* Req)
-	{
-		if (Req->get_billAmountTracker() <= 10 && Req->get_billAmountTracker() <= 50)
-		{
-			cout << "CSR Processing Amount $" << Req->get_billAmountTracker() << endl;
-		}
-		else
-			NextObj->billProcessor(Req);	
-	}
-	
-};
-class Supervisor_CSR : public billApprovalchain {
-private:
-	billApprovalchain* NextObj;
-public:
-	void setNext(billApprovalchain* obj)
-	{
-		NextObj = obj;
-	}
-	void billProcessor(billAmountTracker* Req)
-	{
-		if (Req->get_billAmountTracker()>50 && Req->get_billAmountTracker() <= 100)
-		{
-			cout << "Supervisor CSR Processing Amount $" << Req->get_billAmountTracker() << endl;
-		}
-		else
-			NextObj->billProcessor(Req);
-	}
-
-};
-class SeniorSupervisor_CSR : public billApprovalchain {
-private:
-	billApprovalchain* NextObj;
-public:
-	void setNext(billApprovalchain* obj)
-	{
-		NextObj = obj;
-	}
-	void billProcessor(billAmountTracker* Req)
-	{
-		if (Req->get_billAmountTracker() > 100)
-		{
-			cout << "Senior Supervisor CSR Processing Amount $" << Req->get_billAmountTracker() << endl;
-		}
-		else
-			cout<<"Error: No one is here to Handle the Session request";
-	}
-
-};
-int main() {
-	billApprovalchain* nObj= new Normal_CSR();
-	billApprovalchain* sObj = new Supervisor_CSR();
-	billApprovalchain* SensObj = new SeniorSupervisor_CSR();
-	int n;
-	cout << "Dear employee, Please enter the amount to reimburse: " << endl;
-	cout << "Amount: $ ";
-	cin >> n;
-	nObj->setNext(sObj);
-	sObj->setNext(SensObj);
-	
-	
-	billAmountTracker* obj1 = new billAmountTracker(n);
-	nObj->billProcessor(obj1);
-
-	cout << "Amount $" << obj1->get_billAmountTracker() << "  has been processed" << endl;
-	return 0;
+ 
+int main()
+{
+    int i = 2; // Initialization expression
+ 
+    do {
+        // loop body
+        cout << "Hello World\n";
+ 
+        // update expression
+        i++;
+ 
+    } while (i < 1); // test expression
+ 
+    return 0;
 }
